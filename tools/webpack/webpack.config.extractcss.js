53d7062acdbaa9f4e78d371598965bd90471186b
Andrea Carraro <me@andreacarraro.it>
2019-02-09 18:27:13 +0100

    Fix CSS loader

--- tools/webpack/webpack.config.extractcss.js
+++ tools/webpack/webpack.config.extractcss.js
@@ -3,8 +3,9 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = (env = {}) => {
-  const devMode = process.env.NODE_ENV !== 'production' || env.target === 'server';
+  const devMode = process.env.NODE_ENV !== 'production';
   const targetServer = env.target === 'server';
+  const useStyleLoader = devMode && targetServer === false;
 
   return {
     module: {
@@ -12,13 +13,12 @@ module.exports = (env = {}) => {
         {
           test: /\.css$/,
           use: [
-            (devMode && targetServer === false) ? 'style-loader' : MiniCssExtractPlugin.loader,
+            useStyleLoader ? 'style-loader' : MiniCssExtractPlugin.loader,
             {
               loader: 'css-loader',
               options: {
                 sourceMap: true,
                 modules: false,
-                localIdentName: '[local]-[hash:base62:8]',
                 import: false,
               },
             },
@@ -28,14 +28,10 @@ module.exports = (env = {}) => {
       ],
     },
     plugins: [
-      // new ExtractTextPlugin({
-      //   filename: '[name]-[contenthash:base62:8].css',
-      //   allChunks: true,
-      // }),
       new MiniCssExtractPlugin({
-        filename: "[name]-[contenthash:base62:8].css'",
-        chunkFilename: "[id].css",
-      })
+        filename: useStyleLoader ? '[name].css' : '[name].[hash].css',
+        chunkFilename: useStyleLoader ? '[id].css' : '[id].[hash].css',
+      }),
     ],
   };
 };
